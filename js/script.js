2d183165c5db32904e2d6b643e3d2cdf5ae4594e
rizwan <rizwanakramyt@gmail.com>
2024-01-12 20:57:24 +0500

    7 Screens Done

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
-fetch("./header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
 
-fetch("./footer.html")
+document.addEventListener("DOMContentLoaded", function () {
+  // Code to be executed when the DOM is ready
+  console.log("DOM is ready");
+
+  fetch("./header.html")
     .then(response => {
-        return response.text()
+      return response.text()
     })
     .then(data => {
-        document.querySelector("footer").innerHTML = data;
+      document.querySelector("header").innerHTML = data;
+      console.log("Header Loaded");
+    }).then(function () {
+      const sidebar = document.getElementById("sidebar");
+      const showSidebar = document.getElementById("showSidebar").addEventListener("click", function () {
+        sidebar.style.display = "block";
+      });
+    }).then(function () {
+      const sidebar = document.getElementById("sidebar");
+      const hideSidebar = document.getElementById("hideSidebar").addEventListener("click", function () {
+        sidebar.style.display = "none";
+      });
+    }).then(function () {
+      const search = document.getElementById("search");
+      const searchBtn = document.getElementById("searchBtn");
+      searchBtn.addEventListener("click", function () {
+        searchBtn.style.display = "none";
+        search.style.display = "block";
+
+      })
     });
 
-    
-    $( document ).ready(function() {
-      console.log( "document loaded" );
-  });
+  fetch("./footer.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footer").innerHTML = data;
+      console.log("Footer Loaded");
+    });
 
-  $( window ).on( "load", function() {
-      console.log( "window loaded" );
-  });
-  document.getElementById("demo").addEventListener("click", myFunction);
-  
-  function myFunction() {
-      console.log("clicked");
-    document.getElementById("sidebar").style.display = "block";
-  }
+
+
+
+});
